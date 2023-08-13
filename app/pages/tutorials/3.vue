7dd534bee2ac221b8881e7474034a908cecbbe94
lebrancconvas <lebrancconvas@gmail.com>
2023-08-13 23:34:31 +0700

    ✨ feat: tutorial 03

---
+++ app/pages/tutorials/3.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div id="main">
+      <h1 :style="{color: color}">{{ message }}</h1>
+    </div>
+    <div id="color-controller">
+      <v-btn color="green" @click="changeColor('green')">Green</v-btn>
+      <v-btn color="yellow" @click="changeColor('yellow')">Yellow</v-btn>
+      <v-btn color="orange" @click="changeColor('orange')">Orange</v-btn>
+      <v-btn color="white" @click="changeColor('white')">Reset</v-btn>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Placeholder',
+      color: 'white'
+    }
+  },
+  methods: {
+    changeColor(color) {
+      this.color = color;
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
