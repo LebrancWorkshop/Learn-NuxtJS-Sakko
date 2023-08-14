8310651ce409865588200698b4acc6ff168924e9
lebrancconvas <lebrancconvas@gmail.com>
2023-08-14 14:58:05 +0700

    ✨ feat: updates

---
+++ app/pages/tutorials/computedmethods.vue
@@ -0,0 +1,45 @@
+<!-- Methods รับค่า Argument ได้ แต่ Computed ทำไม่ได้  -->
+<!-- ค่าใน Computed หากค่าไม่เปลี่ยน จะไม่ Run ใหม่ หากมีการ Re-Render แต่ค่าใน Methods จะมีการรันใหม่ เมื่อมีการ Re-Render  -->
+<template>
+  <div>
+    <p>Message: {{ message }}</p>
+    <p>Computed: {{ computedMessage }}</p>
+    <p>Methods: {{ methodMessage() }}</p>
+    <p>{{ fullName }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        message: 'Example Text',
+        firstName: 'John',
+        lastName: 'Spagehtti'
+      }
+    },
+    computed: {
+      computedMessage() {
+        return this.message.split(' ').reverse().join(' ');
+      },
+      fullName: {
+        get() {
+          return `${this.firstName} ${this.lastName}`;
+        },
+        set(val) {
+          this.firstName = val.split(' ')[0];
+          this.lastName = val.split(' ')[1];
+        }
+      }
+    },
+    methods: {
+      methodMessage() {
+        return this.message.split(' ').reverse().join(' ');
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ app/pages/tutorials/watcher.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div id="question">
+      <h1>Question</h1>
+      <v-text-field class="inputQuestion" type="text" v-model="question"></v-text-field>
+    </div>
+    <div id="answer">
+      <p>{{ answer }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        question: '',
+        answer: ''
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .inputQuestion {
+    width: 30%;
+  }
+</style>
