151367770fa9fa5a638ab01f868eb55aa8710aba
lebrancconvas <lebrancconvas@gmail.com>
2023-08-14 15:36:51 +0700

    ✨ feat: create login form for tutorial

---
+++ app/components/LoginForm.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div id="login">
+      <label for="username">Username</label>
+      <v-text-field type="text" placeholder="Enter your username" name="username" v-model="username" />
+
+      <label for="password">Password</label>
+      <v-text-field type="password" placeholder="Enter your password" name="password" v-model="password" />
+
+      <v-btn color="green" @click="handleLogin">Login</v-btn>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    handleLogin() {
+      console.log('Handle Login Success');
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ app/pages/tutorials/emit.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <header>
+      <h1>Login Form</h1>
+    </header>
+    <div>
+      <LoginForm />
+    </div>
+  </div>
+</template>
+
+<script>
+import LoginForm from '@/components/LoginForm.vue';
+export default {
+  components: {
+    LoginForm
+  },
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
